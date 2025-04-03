8cbd589c35d8d3bbfe1bda91b0530751f6f713d3
milicas15 <mslavkov@purdue.edu>
2025-04-02 21:47:04 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -20,19 +20,39 @@ public class Product {
         return productID;
     }
 
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public double getPrice() {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public String getCategory() {
         return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
