0eeab4e6757d6f9312618f34afcf8987247d6ca2
savirpatil <74161605+savirpatil@users.noreply.github.com>
2025-04-06 14:06:54 -0400

    Update IMessageService.java - Added comments

--- IMessageService.java
+++ IMessageService.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
-
+/**
+ * Interface that includes the actions that can be done with the use of Message objects, primarily sending/deleting.
+ * 
+ * Purdue Unversity -- CS180 -- Spring 2025 -- Team Project 
+ * @author Savir Patil
+ * @version April 6, 2025
+ */
 public interface IMessageService {
     void sendMessage(int senderId, int receiverId, String messageContent);
     void deleteMessage(int messageId);
