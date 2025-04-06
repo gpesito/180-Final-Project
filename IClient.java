52455b2f7a49672c42127be61a9ee9ef17e039e0
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-06 13:31:08 -0400

    Update IClient.java

--- IClient.java
+++ IClient.java
@@ -1,7 +1,7 @@
 /**
  * This is the interface for Client.java
  *
- * <p>Purdue University -- CS18000 -- Fall 2025 -- Team Project
+ * <p>Purdue University -- CS18000 -- Spring 2025 -- Team Project
  *
  * @author Jay Saini Purdue CS
  * @version April 6, 2025
