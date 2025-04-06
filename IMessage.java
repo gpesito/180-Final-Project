409833c1b7a8f62c9b87b7cab38628b0d6243cac
savirpatil <74161605+savirpatil@users.noreply.github.com>
2025-04-06 14:05:44 -0400

    Update IMessage.java - Added comments

--- IMessage.java
+++ IMessage.java
@@ -1,3 +1,10 @@
+/**
+ * Interface that defines the blueprint for a Message object and methods that are used to acces important fields.
+ * 
+ * Purdue Unversity -- CS180 -- Spring 2025 -- Team Project 
+ * @author Savir Patil
+ * @version April 6, 2025
+ */
 public interface IMessage {
     int getMessageId();
     int getSenderId();
