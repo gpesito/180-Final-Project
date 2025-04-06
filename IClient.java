08dda6618c4d947ca0f3248cabdba1a9370b6d30
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-06 13:29:55 -0400

    Create IClient.java

---
+++ IClient.java
@@ -0,0 +1,11 @@
+/**
+ * This is the interface for Client.java
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2025 -- Team Project
+ *
+ * @author Jay Saini Purdue CS
+ * @version April 6, 2025
+ */
+public interface IClient {
+void start ();
+}
