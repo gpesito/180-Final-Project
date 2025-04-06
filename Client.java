89185309327e407cb00d85de258f476a73194bca
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-06 16:45:05 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -11,7 +11,7 @@ import services.*; //imports our service classes
 
 public class Client implements IClient {
     private final MarketplaceServer server;
-
+//the constructor that accepts MarketplaceServer instances
     public Client(MarketplaceServer server) {
         this.server = server;
     }
@@ -29,6 +29,7 @@ public class Client implements IClient {
             new TransactionService(),
             new MessageService()
         );
+        //launches the client
         new Client(server).start();
     }
 }
