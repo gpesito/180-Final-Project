75837e3dc6a9a40625349053a20f5aac6ad8d960
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-06 13:34:08 -0400

    Update MarketplaceServer.java

--- MarketplaceServer.java
+++ MarketplaceServer.java
@@ -1,10 +1,18 @@
 package server;
-//MarketplaceServer class - Jay
-
-
 import services.*; //imports our other service classes
 
-public class MarketplaceServer {
+/**
+ * This is the MarketplaceServer class.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2025 -- Team Project
+ *
+ * @author Jay Saini Purdue CS
+ * @version April 6, 2025
+ */
+
+
+
+public class MarketplaceServer implements IMarketplaceServer {
     public UserService users;
     public ProductService products;
     public TransactionService transactions;
@@ -15,5 +23,38 @@ public class MarketplaceServer {
         this.products = p;
         this.transactions = t;
         this.messages = m;
+    }
+
+  
+    @Override
+    public boolean registerUser(String username, String email, String password) {
+        return users.register(username, email, password);
+    }
+
+    @Override
+    public boolean loginUser(String username, String password) {
+        return users.login(username, password);
+    }
+
+
+    
+    @Override
+    public boolean addProduct(String name, double price, String description, int sellerId) {
+        return products.addProduct(name, price, description, sellerId);
+    }
+
+    @Override
+    public List<Product> searchProducts(String keyword) {
+        return products.searchProducts(keyword);
+    }
+
+    @Override
+    public boolean createTransaction(int buyerId, int productId) {
+        return transactions.processTransaction(buyerId, productId);
+    }
+
+    @Override
+    public boolean sendMessage(int senderId, int receiverId, String content) {
+        return messages.sendMessage(senderId, receiverId, content);
     }
 }
