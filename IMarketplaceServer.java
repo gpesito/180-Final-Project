244976c240d2dc6f23710d598a6f07676bf08db1
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-06 13:30:58 -0400

    Update IMarketplaceServer.java

--- IMarketplaceServer.java
+++ IMarketplaceServer.java
@@ -1,5 +1,12 @@
 package server;
-
+/**
+ * This is the interface for MarketplaceServer.java
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2025 -- Team Project
+ *
+ * @author Jay Saini Purdue CS
+ * @version April 6, 2025
+ */
 import java.util.List;
 import models.Product;
 
