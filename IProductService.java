cee277b5f7a42f6288258e08cb6042f917fde9cd
milicas15 <mslavkov@purdue.edu>
2025-04-06 15:55:53 -0400

    Update IProductService.java

--- IProductService.java
+++ IProductService.java
@@ -1,5 +1,14 @@
 import java.util.List;
 
+/**
+ * Interface that defines the contract for ProductService class to manage products
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2025</p>
+ *
+ * @author Milica Slavkovic
+ * @version April 6, 2025
+ */
+
 public interface IProductService {
     void addProduct(String productID, String name, String description, double price, String category);
     void deleteProduct(String productID);
