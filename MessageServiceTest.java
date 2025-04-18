d07ce9acc95c269dfe36a2de1d9cf010fa9a3295
savirpatil <74161605+savirpatil@users.noreply.github.com>
2025-04-18 17:28:30 -0400

    Update MessageServiceTest.java - Phase 2 Changes

--- MessageServiceTest.java
+++ MessageServiceTest.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
  * 
  * Purdue Unversity -- CS180 -- Spring 2025 -- Team Project 
  * @author Savir Patil
- * @version April 6, 2025
+ * @version April 18, 2025
  */
 public class MessageServiceTest {
-    public MessageService messageService;
+    private MessageService messageService;
 
     @Before
     public void setUp() {
@@ -19,32 +19,33 @@ public class MessageServiceTest {
 
     @Test
     public void testSendMessage() {
-        messageService.sendMessage(111, 222, "Hello!");
+        messageService.sendMessage("111", "222", "Hello!", "product1");
 
-        ArrayList<Message> messages = messageService.getAllMessages(111);
+        ArrayList<Message> messages = messageService.getAllMessages("111");
         assertEquals("Message count is incorrect after sending!", 1, messages.size());
         assertEquals("Message content does not match expected!", "Hello!", messages.get(0).getMessageContent());
+        assertEquals("Sender ID does not match expected!", "111", messages.get(0).getSenderId());
     }
 
     @Test
     public void testDeleteMessage() {
-        messageService.sendMessage(111, 222, "Message to delete");
-        ArrayList<Message> messagesBefore = messageService.getAllMessages(111);
+        messageService.sendMessage("111", "222", "Message to delete", "product2");
+        ArrayList<Message> messagesBefore = messageService.getAllMessages("111");
         assertEquals("Message count should be 1 before deletion!", 1, messagesBefore.size());
 
         messageService.deleteMessage(messagesBefore.get(0).getMessageId());
 
-        ArrayList<Message> messagesAfter = messageService.getAllMessages(111);
+        ArrayList<Message> messagesAfter = messageService.getAllMessages("111");
         assertEquals("Message was not deleted correctly!", 0, messagesAfter.size());
     }
 
     @Test
     public void testGetMessagesForUser() {
-        messageService.sendMessage(101, 202, "First message");
-        messageService.sendMessage(101, 202, "Second message");
-        messageService.sendMessage(103, 202, "Another user’s message");
+        messageService.sendMessage("101", "202", "First message", "product3");
+        messageService.sendMessage("101", "202", "Second message", "product4");
+        messageService.sendMessage("103", "202", "Another user’s message", "product5");
 
-        ArrayList<Message> messages = messageService.getAllMessages(101);
+        ArrayList<Message> messages = messageService.getAllMessages("101");
 
         assertEquals("Incorrect number of messages retrieved for user!", 2, messages.size());
         assertEquals("First message content is incorrect!", "First message", messages.get(0).getMessageContent());
