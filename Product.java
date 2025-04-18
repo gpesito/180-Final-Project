e2bddf260ea1acdc3c152ebacfb16cd335ba825e
milicas15 <mslavkov@purdue.edu>
2025-04-18 14:55:52 -0400

    Product.java

--- Product.java
+++ Product.java
@@ -1,26 +1,38 @@
+import java.io.Serializable;
+
 /**
  * This class represents a product in an inventory
  *
  * <p>Purdue University -- CS18000 -- Spring 2025</p>
  *
  * @author Milica Slavkovic
- * @version April 6, 2025
+ * @version April 18, 2025
  */
 
-public class Product {
+public class Product implements IProduct, Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String productID; // Unique identifier for the product
     private String name; // Name of the product
     private String description; // Detailed description of the product
     private double price; // Price of the product
     private String category; // Category to which the product belongs
-    
-// Constructor to initialize all fields of the Product
-    public Product(String productID, String name, String description, double price, String category) {
+    private String sellerId; // The ID of the seller
+
+    // Constructor to initialize all fields of the Product
+    public Product(String productID, String name, String description, double price,
+                   String category, String sellerId) {
         this.productID = productID;
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
+        this.sellerId = sellerId;
+    }
+
+    // Legacy constructor
+    public Product(String productID, String name, String description, double price, String category) {
+        this(productID, name, description, price, category, "");
     }
 
     // Getters and setters
@@ -62,5 +74,25 @@ public class Product {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+               "productID='" + productID + '\'' +
+               ", name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               ", price=" + price +
+               ", category='" + category + '\'' +
+               ", sellerId='" + sellerId + '\'' +
+               '}';
     }
 }
