846cbdb77455b35d119223d66bf7d6649366a970
savirpatil <74161605+savirpatil@users.noreply.github.com>
2025-04-18 17:22:48 -0400

    Update MessageTest.java - Phase 2 Changes

--- MessageTest.java
+++ MessageTest.java
@@ -5,25 +5,28 @@ import org.junit.Test;
  * 
  * Purdue Unversity -- CS180 -- Spring 2025 -- Team Project 
  * @author Savir Patil
- * @version April 6, 2025
+ * @version April 18, 2025
  */
 public class MessageTest {
     @ Test
     public void testNewMessage() {
-        Message message = new Message(1, 100, 200, "Test Message");
+        LocalDateTime timestamp = LocalDateTime.now();
+        Message message = new Message("1", "100", "200", "300", "Test Message", timestamp);
 
-        assertEquals("Message ID is incorrect!", 1, message.getMessageId());
-        assertEquals("Sender ID is incorrect!", 100, message.getSenderId());
-        assertEquals("Receiver ID is incorrect!", 200, message.getReceiverId());
+        assertEquals("Message ID is incorrect!", "1", message.getMessageId());
+        assertEquals("Sender ID is incorrect!", "100", message.getSenderId());
+        assertEquals("Receiver ID is incorrect!", "200", message.getReceiverId());
+        assertEquals("Product ID is incorrect!", "300", message.getProductId());
         assertEquals("Message content is incorrect!", "Test Message", message.getMessageContent());
+        assertEquals("Timestamp is incorrect!", timestamp, message.getTimestamp());
 
     }
 
     @ Test
     public void testSetContent() {
-        Message message = new Message(2, 101, 201, "Initial Message");
+        LocalDateTime timestamp = LocalDateTime.now();
+        Message message = new Message("2", "101", "201", "301", "Initial Message", timestamp);
         message.setMessageContent("New Message");
-
         assertEquals("Content wasn't updated correctly ", "New Message", message.getMessageContent());
     }
 }
