79c49fec1349d5ff60134851a3c7a25aab7babbe
savirpatil <74161605+savirpatil@users.noreply.github.com>
2025-04-18 17:07:17 -0400

    Update MessageService.java - Phase 2 Changes

--- MessageService.java
+++ MessageService.java
@@ -1,22 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.time.LocalDateTime;
 /**
  * Class that allows user to send, delete, or view alll messages by a specific user.
  * 
  * Purdue Unversity -- CS180 -- Spring 2025 -- Team Project 
  * @author Savir Patil
- * @version April 6, 2025
+ * @version April 18, 2025
  */
 public class MessageService implements IMessageService {
     private ArrayList<Message> messages;
     private int messageCount;
+    
 
     public MessageService() {
-        this.messages = new ArrayList<Message>();
+        this.messages = Collections.synchronizedList(new ArrayList<>());
+        this.messageCounter = 0;
     }
     // Sends new message and adds message to list that contains all messages.
     @Override
-    public void sendMessage(int senderId, int receiverId, String messageContent) {
-        Message message = new Message(messageCount++, senderId, receiverId, messageContent);
+    public void sendMessage(String senderId, String receiverId, String messageContent) {
+        Message message = new Message(messageCount++, senderId, receiverId, messageContent, LocalDateTime.now());
         messages.add(message);
     }
     // Deletes message from "storage" in our list.
@@ -32,9 +36,11 @@ public class MessageService implements IMessageService {
     // Uses a specific user's ID to access messages they have sent. 
     public ArrayList<Message> getAllMessages(int userId) {
         ArrayList<Message> userMessages = new ArrayList<>();
-        for (Message message : messages) {
-            if (message.getSenderId() == userId || message.getReceiverId() == userId) {
-                userMessages.add(message);
+        synchronized (message) {
+            for (Message message : messages) {
+                if (message.getSenderId() == userId || message.getReceiverId() == userId) {
+                    userMessages.add(message);
+                }
             }
         }
         return userMessages;
