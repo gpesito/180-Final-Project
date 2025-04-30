ecd781eb0bb8e1afed543bbd32b41a1ac19479c3
Jay Saini <66807873+jaysaini06@users.noreply.github.com>
2025-04-30 09:05:11 -0400

    Update IClient.java

--- IClient.java
+++ IClient.java
@@ -1,23 +1,27 @@
+import java.io.IOException;
 /**
  * IClient interface defines the contract for client-side operations
  * in a client-server communication model.
  *
  * <p>Purdue University -- CS18000 -- Spring 2025</p>
  * Jay Saini @author
- * @version April 20, 2025
+ * @version April 30, 2025
  */
+
 public interface IClient {
 
 
-    void connect(String serverAddress, int serverPort) throws Exception;
+    void connect(String serverAddress, int serverPort) throws IOException;
 
 
-    void sendCommand(String command) throws Exception;
+    void sendCommand(String command) throws IOException;
 
 
-    String readResponse() throws Exception;
+    String readResponse() throws IOException;
 
 
-    void disconnect() throws Exception;
+    void disconnect() throws IOException;
+
+
+    void launchGUI();
 }
-
